3c4ce1d12b474fcccdebd2e52315256c0f4687c8
Ashwin Saklecha <ashwinsaklecha2004@gmail.com>
2024-06-21 11:23:56 +0530

    chore: Add input field and counter functionality to App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [input, setInput] = useState("");
+  const [count, setCount] = useState(0);
+  const incrementCount = () => setCount(count + 1);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={incrementCount}>Increment Counter</button>
+
+
+      <h3></h3>
+
+      <h3>Counter: {count}</h3>
+
+      <ChildComponent count={count} onClick={incrementCount} />
     </div>
   );
 }
+
+const ChildComponent = React.memo(function ({ count, onClick }) {
+  console.log("Child Component Rendered");
+  return (
+    
+    <div>
+      <h1>{count}</h1>
+      <button onClick={onClick}>Increment</button>
+    </div>
+  );
+});
 
 export default App;
